f19f57414b118c090f64be1296b9defc7aea8d8d
Александр Пичугин <aleksandrpicugin700@gmail.com>
2025-03-20 21:44:33 +0300

    Insert in progress

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -12,9 +12,10 @@ namespace ADO.NET
     {
         static void Main(string[] args)
         {
+#if INTRO
             //1)Берем строку подключения
             const int PADDING = 30;
-            const string CONNECTION_STRING = 
+            const string CONNECTION_STRING =
                 "Data Source=(localdb)\\MSSQLLocalDB;" +
                 "Initial Catalog=Movies;" +
                 "Integrated Security=True;" +
@@ -30,7 +31,7 @@ namespace ADO.NET
 
             //3) Создаем команду , которую нужно выполнить на сервере
             string cmd = "SELECT title, release_date, FORMATMESSAGE(N'%s %s',first_name, last_name ) FROM Movies, Directors WHERE director=director_id";
-            SqlCommand command= new SqlCommand(cmd, connection);
+            SqlCommand command = new SqlCommand(cmd, connection);
 
             //4) Получаем результаты выполнения команды 
             connection.Open();
@@ -40,14 +41,14 @@ namespace ADO.NET
             if (reader.HasRows)
             {
                 Console.WriteLine("=======================================================================================================");
-                for (int i=0; i<reader.FieldCount; i++)
+                for (int i = 0; i < reader.FieldCount; i++)
                     Console.Write(reader.GetName(i).PadRight(15));
                 Console.WriteLine();
                 Console.WriteLine("======================================================================================================");
-                    while (reader.Read())
+                while (reader.Read())
                 {
-                   // Console.WriteLine($"{reader[0].ToString().PadRight(5)}\t{reader[2].ToString().PadRight(15)}\t{reader[1].ToString().PadRight(15)}");
-                    for (int i=0; i<reader.FieldCount; i++ )
+                    // Console.WriteLine($"{reader[0].ToString().PadRight(5)}\t{reader[2].ToString().PadRight(15)}\t{reader[1].ToString().PadRight(15)}");
+                    for (int i = 0; i < reader.FieldCount; i++)
                     {
                         Console.Write(reader[i].ToString().PadRight(PADDING));
                     }
@@ -56,7 +57,14 @@ namespace ADO.NET
             }
             //6)Закрываем SqlDataReaderr и connections
             reader.Close();
-            connection.Close();
+            connection.Close(); 
+#endif
+
+            // Connector.Select("*", "Directors");
+            //Connector.Select("title,release_date,FORMATMESSAGE(N'%s %s',first_name,last_name)","Movies,Directors","director=director_id");
+            Connector.InsertDirector("George","Martin");
+            Connector.SelectDirectors();
+            Connector.SelectMovies();
         }
     }
 }
